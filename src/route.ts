e1875f93c26596aa3ec7142d7ac26e3f313f294e
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-10-10 09:18:01 -0300

    Alterando o package.json para deixar seu script start apenas para fazer compilar e dar o start

--- src/route.ts
+++ src/route.ts
@@ -3,7 +3,8 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get('/teste',(req:Request, res:Response) => {
-    return res.json({nome: 'Sujeito Pizza'});
+   // return res.json({nome: 'Sujeito Pizza'});
+   throw new Error('Erro ao fazer esta requisição');
 })
 
 export {router};
