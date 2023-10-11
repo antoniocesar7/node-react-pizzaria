f67d420ec321dbbd071ac22f60df3941dd2ef04e
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-10-11 13:46:18 -0300

    configurando controller e serviços e estruturando as pastas

--- src/route.ts
+++ src/route.ts
@@ -1,10 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
-router.get('/teste',(req:Request, res:Response) => {
-   return res.json({nome: 'Sujeito Pizza'});
-   //throw new Error('Erro ao fazer esta requisição');
-})
+//-- ROTAS USER --
+router.post('/users', new CreateUserController().handle);
 
 export {router};
