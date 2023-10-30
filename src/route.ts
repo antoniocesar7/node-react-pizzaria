8bbb5936b0952660f1e5ae165e1562414ba2580f
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-10-30 11:01:53 -0300

    Criando o serviço e controller para criar as categorias

--- src/route.ts
+++ src/route.ts
@@ -4,6 +4,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/Category/CreateCategoryController";
 
 
 const router = Router();
@@ -14,5 +15,9 @@ router.post('/users',   new CreateUserController().handle);
 router.post('/session', new   AuthUserController().handle);
 
 router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+//-- ROTAS CATEGORY
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 export {router};
